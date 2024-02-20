9edafb83b863bdd772091366ce3e37044e4efd2c
Mak Ludovic <ludovic93mak@gmail.com>
2024-02-20 10:37:41 +0100

    correction de la nommation dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DashbordController extends AbstractController
+class DashboardController extends AbstractController
 {
-    #[Route('/eleve/dashbord', name: 'app_home')]
+    #[Route('/eleve/dashboard', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'DashbordController',
+            'controller_name' => 'DashboardController',
         ]);
     }
 }
